51a94535cda0b86643b1531b906eaac3df2564f3
prabhu227 <prabhuprasadpanda56@gmail.com>
2025-05-04 21:19:27 +0530

    demo

--- spanc/assets/JS/product.js
+++ spanc/assets/JS/product.js
@@ -1,19 +1,3 @@
-
-    // Toggle sidebar on small screens
-    const sidebarToggle = document.getElementById('sidebarToggle');
-    const sidebar = document.getElementById('sidebar');
-    const overlay = document.getElementById('overlay');
-
-    sidebarToggle.addEventListener('click', () => {
-      sidebar.classList.toggle('show');
-      overlay.classList.toggle('show');
-    });
-
-    overlay.addEventListener('click', () => {
-      sidebar.classList.remove('show');
-      overlay.classList.remove('show');
-    });
-
     function toggleDropdown(id) {
     const element = document.getElementById(id);
     if (element) {
